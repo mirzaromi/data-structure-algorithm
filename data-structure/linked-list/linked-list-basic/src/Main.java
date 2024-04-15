1b7f40a279827f5bebe5c78b68f67b4f0cd87fe7
mirz <mirza.romi@doku.com>
2024-04-15 17:28:57 +0700

    move Node to a new class

--- data-structure/linked-list/linked-list-basic/src/Main.java
+++ data-structure/linked-list/linked-list-basic/src/Main.java
@@ -4,6 +4,11 @@ public class Main {
         singlyLinkedList.addNode(1);
         singlyLinkedList.addNode(2);
         singlyLinkedList.addNode(3);
+        singlyLinkedList.addNode(4);
+        singlyLinkedList.addNode(5);
+        singlyLinkedList.addNode(6);
+
+
         singlyLinkedList.display();
     }
 }
--- data-structure/linked-list/linked-list-basic/src/SinglyLinkedList.java
+++ data-structure/linked-list/linked-list-basic/src/SinglyLinkedList.java
@@ -29,12 +29,4 @@ public class SinglyLinkedList {
 
 }
 
-class Node<T> {
-    public T data;
-    public Node<T> next;
 
-    public Node(T data) {
-        this.data = data;
-        this.next = null;
-    }
-}
