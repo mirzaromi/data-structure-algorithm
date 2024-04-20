8e41479dbfd5e2eacfb86ad1c813dc39fac7ccbe
mirz <mirza.romi@doku.com>
2024-04-20 11:29:51 +0700

    call DoublyLinkedList in main function

--- data-structure/linked-list/doubly-linked-list/src/Main.java
+++ data-structure/linked-list/doubly-linked-list/src/Main.java
@@ -1,5 +1,15 @@
+import doublyLinkedList.DoublyLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DoublyLinkedList linkedList = new DoublyLinkedList();
+        linkedList.addNodeFromTail(1);
+        linkedList.addNodeFromTail(2);
+        linkedList.addNodeFromTail(3);
+        linkedList.addNodeFromTail(4);
+        linkedList.addNodeFromTail(5);
+
+        linkedList.display();
+
     }
 }
