d932b839160a96ee76b3a4656a2f51c1f8cb50d6
mirz <mirza.romi@doku.com>
2024-04-20 11:17:49 +0700

    create node class

---
+++ data-structure/linked-list/doubly-linked-list/src/doublyLinkedList/Node.java
@@ -0,0 +1,36 @@
+package doublyLinkedList;
+
+public class Node<T> {
+    private T data;
+    private Node prev;
+
+    private Node next;
+
+    public Node(T data) {
+        this.data = data;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
