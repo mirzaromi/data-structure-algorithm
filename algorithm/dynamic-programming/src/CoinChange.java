42d90baf8072da663d5cb1c35e017aa7ba352f0f
mirz <mirza.romi@doku.com>
2024-04-30 13:57:49 +0700

    add memoization

--- algorithm/dynamic-programming/src/CoinChange.java
+++ algorithm/dynamic-programming/src/CoinChange.java
@@ -1,29 +1,39 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CoinChange {
     public static void main(String[] args) {
         // coin problem
         // given list of coin with value [1,4,5]
         // find how many minimum total coin needed with the sum is 13
-        int[] coins = {1,4,5};
-        int sumValue = 50;
-        System.out.println("the answer is "+findMinimumCoin(coins, sumValue));
+        int[] coins = {2,3,5};
+        int sumValue = 7;
+        Map<Integer, Integer> memoization = new HashMap<>();
+        System.out.println("the answer is "+findMinimumCoin(coins, sumValue, memoization));
+        System.out.println(memoization.toString());
 
     }
 
     // top down
-    public static int findMinimumCoin(int[] coins, int sumValue) {
-        int minimumCoin = Integer.MAX_VALUE;
+    public static int findMinimumCoin(int[] coins, int sumValue, Map<Integer, Integer> memoization) {
+        int minimumCoin = Integer.MAX_VALUE-1;
         if (sumValue == 0) {
             return 0;
         }
 
+        int subProblem = 0;
         for (int coin: coins) {
-            int subProblem = sumValue - coin;
+            subProblem = sumValue - coin;
             if (subProblem < 0) {
                 continue;
             }
-            minimumCoin = Math.min(minimumCoin, findMinimumCoin(coins, subProblem)+1);
+            if (memoization.containsKey(sumValue)) {
+                minimumCoin = memoization.get(sumValue);
+            } else {
+                minimumCoin = Math.min(minimumCoin, findMinimumCoin(coins, subProblem, memoization)+1);
+            }
         }
-
+        memoization.putIfAbsent(sumValue, minimumCoin);
 
         return minimumCoin;
     }
